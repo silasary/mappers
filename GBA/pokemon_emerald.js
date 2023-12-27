19294b9092823bec194afc515239ebf25957cdeb
OttoTonsorialist <109401851+OttoTonsorialist@users.noreply.github.com>
2023-12-27 11:29:26 -0500

    fixing minor issues with battle properties. Adding sound effect properties found by Waiel. Fixing/adding bag mapping. Added better handling to js for resets, and doing minor verification on blocks before releasing

--- GBA/pokemon_emerald.js
+++ GBA/pokemon_emerald.js
@@ -104,16 +104,36 @@ function preprocessor() {
 
     if (state.cached_dma_a === undefined) {
         state.dma_update_delay = 0;
+        state.blocked_last_frame = false;
     } else if (
-            state.cached_dma_a != variables.dma_a ||
-            state.cached_dma_b != variables.dma_b ||
-            state.cached_dma_c != variables.dma_c ||
-            state.cached_quantity_decryption_key != variables.quantity_decryption_key
-    ){
-        console.log("DMA change detected/expected");
+        // If any of the new values are 0, that indicates the player is resetting. Allow updates
+        // Additionally, if any of the cahed values are 0, that indicates the file is loading after a reset. Allow updates
+        variables.dma_a == 0 ||
+        variables.dma_b == 0 ||
+        variables.dma_c == 0 ||
+        state.cached_dma_a == 0 ||
+        state.cached_dma_b == 0 ||
+        state.cached_dma_c == 0
+    ) {
+        if (state.dma_update_delay != 0) {
+            console.log("reset detected, allowing messy updates");
+        }
+        // forcibly disable dma updates, to be sure
+        state.dma_update_delay = 0;
+    } else if (
+        // Once we know we aren't resetting, check to see if the dma values are shifting. If so, block updates
+        state.cached_dma_a != variables.dma_a ||
+        state.cached_dma_b != variables.dma_b ||
+        state.cached_dma_c != variables.dma_c ||
+        state.cached_quantity_decryption_key != variables.quantity_decryption_key
+    ) {
+        // DMA is actually changing, and not due to a reset. Begin blocking
         state.dma_delay_init = getTotalGameTime();
-        state.dma_update_delay = getTotalGameTime() + 90;
+        state.dma_update_delay = getTotalGameTime() + 60;
+        state.dma_safety_delay = getTotalGameTime() + 300;
+        console.log("DMA change detected, enabling block until changes are complete");
     }
+
 
     state.cached_dma_a = variables.dma_a;
     state.cached_dma_b = variables.dma_b;
@@ -123,25 +143,62 @@ function preprocessor() {
     if (state.dma_update_delay != 0){
         let game_time = getTotalGameTime();
         if (state.dma_update_delay > game_time){
+            state.blocked_last_frame = true;
             return false;
         } else if (state.dma_delay_init > game_time) {
             console.log("Impossible game_time detected. Assuming player reset or loaded a save state. Lifting block");
             state.dma_update_delay = 0;
         } else {
+            // grab the raw value of the new items. If either is correct, then we can safely assume that the DMA block has been moved successfully
+            // using 2 values safeguards against weird cases where the player is somehow able to make a change to the actual data before gamehook detects the new changes
+            // if BOTH new values fail to meet the cache, then assume the DMA is still not updated properly, and continue to block
+            if (
+                state.cached_first_item_sanity_check !== undefined &&
+                state.cached_second_item_sanity_check !== undefined &&
+                state.cached_first_item_sanity_check != memory.defaultNamespace.get_uint16_le(variables.dma_a + 1376) &&
+                state.cached_second_item_sanity_check != memory.defaultNamespace.get_uint16_le(variables.dma_a + 1380)
+            ){
+                if (state.dma_safety_delay > game_time) {
+                    state.blocked_last_frame = true;
+                    return false;
+                } else {
+                    console.log("New item values disagree with cache for first item: " + state.cached_first_item_sanity_check + " vs. " + memory.defaultNamespace.get_uint16_le(variables.dma_a + 1376));
+                    console.log("New item values disagree with cache for second item: " + state.cached_second_item_sanity_check + " vs. " + memory.defaultNamespace.get_uint16_le(variables.dma_a + 1380));
+                    console.log("DMA safety delay timed out. Allowing updates despite mismatching data logged above");
+                }
+            } else if (
+                state.cached_player_id !== undefined &&
+                state.cached_player_id != memory.defaultNamespace.get_uint16_le(variables.dma_b + 10)
+            ) {
+                if (state.dma_safety_delay > game_time) {
+                    state.blocked_last_frame = true;
+                    return false;
+                } else {
+                    console.log("Cached player id disagrees with new player ID: " + state.cached_first_item_sanity_check + " vs. " + memory.defaultNamespace.get_uint16_le(variables.dma_a + 1376));
+                    console.log("DMA safety delay timed out. Allowing updates despite mismatching data logged above");
+                }
+            }
             console.log("DMA should be ready now, lifting block on updates");
             state.dma_update_delay = 0;
         }
     }
 
+    // when we know we have good data
+    // cache 3 pieces of data to guarantee integrity. Player ID, plus 
+    // cache 2 separate items to sanity check for cases where DMA update takes longer than expected
+    state.cached_player_id = memory.defaultNamespace.get_uint16_le(variables.dma_b + 10)
+    state.cached_first_item_sanity_check = memory.defaultNamespace.get_uint16_le(variables.dma_a + 1376);
+    state.cached_second_item_sanity_check = memory.defaultNamespace.get_uint16_le(variables.dma_a + 1380);
+
     //DECRYPTION OF THE PARTY POKEMON
     //This process applies to all the the Player's Pokemon as well as to Pokemon loaded NPCs parties
     //All Pokemon have a data structure of 100-bytes
     //Only 48-bytes of data are encrypted and shuffled in generation 3
-    const partyStructures = ["player", "opponentA"];
+    const partyStructures = ["player", "opponent"];
     if (state.cached_pokemon === undefined) {
         state.cached_pokemon = {
             "player": {0: {raw_data: "", decrypted_data: ""}, 1: {raw_data: "", decrypted_data: ""}, 2: {raw_data: "", decrypted_data: ""}, 3: {raw_data: "", decrypted_data: ""}, 4: {raw_data: "", decrypted_data: ""}, 5: {raw_data: "", decrypted_data: ""}},
-            "opponentA": {0: {raw_data: "", decrypted_data: ""}, 1: {raw_data: "", decrypted_data: ""}, 2: {raw_data: "", decrypted_data: ""}, 3: {raw_data: "", decrypted_data: ""}, 4: {raw_data: "", decrypted_data: ""}, 5: {key: "", decrypted_data: ""}},
+            "opponent": {0: {raw_data: "", decrypted_data: ""}, 1: {raw_data: "", decrypted_data: ""}, 2: {raw_data: "", decrypted_data: ""}, 3: {raw_data: "", decrypted_data: ""}, 4: {raw_data: "", decrypted_data: ""}, 5: {key: "", decrypted_data: ""}},
         };
         state.blank_pokemon = new Array(100).fill(0x00);
     }
@@ -149,7 +206,7 @@ function preprocessor() {
         let user = partyStructures[i];
 
         let activeMonIndexAddress = 0;
-        if (user == "opponentA") {
+        if (user == "opponent") {
             activeMonIndexAddress = 0x2024070;
         }
 
@@ -159,8 +216,7 @@ function preprocessor() {
             if (user == "player") {
                 startingAddress = 0x20244EC + (100 * slotIndex);
             }
-            // TODO: do we need to handle opponentB as well?
-            if (user == "opponentA") {
+            if (user == "opponent") {
                 startingAddress = 0x2024744 + (100 * slotIndex);
             }
 
@@ -214,6 +270,7 @@ function preprocessor() {
             // NOTE: this can still fail if the player's species gets randomly set to a value under 414. However, this should be very rare
             if (user == "player" && slotIndex == 0 && DATA16_LE(decrypted_data, 32) > 415) {
                 console.log("Junk solo mon data detected. Delaying update...");
+                state.blocked_last_frame = true;
                 return false;
             }
 
@@ -224,16 +281,18 @@ function preprocessor() {
 
         if (activeMonIndexAddress != 0) {
             let activeIndex = memory.defaultNamespace.get_uint16_le(activeMonIndexAddress);
-            console.log("Got active index of ");
-            console.log(activeIndex);
             if (activeIndex < 0 || activeIndex >= 6 ) {
                 memory.fill(`${user}_party_structure_active_pokemon`, 0x00, state.blank_pokemon);
             } else {
-                console.log(state.cached_pokemon[user][activeIndex]["decrypted_data"]);
                 memory.fill(`${user}_party_structure_active_pokemon`, 0x00, state.cached_pokemon[user][activeIndex]["decrypted_data"]);
             }
         }
 
+    }
+
+    if (state.blocked_last_frame){
+        console.log("Block fully lifted until further notice");
+        state.blocked_last_frame = false;
     }
     return true;
 }
