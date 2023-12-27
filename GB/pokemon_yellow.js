49d7209030dac25df8a081a3b5528ff5c7801891
Scotts-Thoughts <99685188+Scotts-Thoughts@users.noreply.github.com>
2023-12-27 13:36:07 -0700

    update to "meta" parent and it's children
    - standardized across all mappers

--- GB/pokemon_yellow.js
+++ GB/pokemon_yellow.js
@@ -75,6 +75,30 @@ function isShiny(ivs) {
     return ivs.defense == 10 && ivs.special == 10 && ivs.speed == 10 && ((ivs.attack & 2) != 0)
 }
 
+function preprocessor() {
+    // FSM FOR GAMESTATE TRACKING
+    // MAIN GAMESTATE: This tracks the three basic states the game can be in.
+    // 1. "No Pokemon": cartridge reset; player has not received a Pokemon
+    // 2. "Overworld": Pokemon in party, but not in battle
+    // 3. "Battle": In battle
+    setPropertyValue('meta.state', 'No Pokemon')
+    if (getPropertyValue('player.team.0.level') == 0) {
+        setPropertyValue('meta.state', 'No Pokemon')
+    }
+    else if (getPropertyValue("battle.type") == "None") {
+        setPropertyValue('meta.state', 'Overworld')
+    }
+    else if (getPropertyValue("battle.turnInfo.battleStart") == 0) {
+        setPropertyValue('meta.state', 'To Battle')
+    }
+    else if (getPropertyValue("battle.lowHealthAlarm") == "Disabled") {
+        setPropertyValue('meta.state', 'From Battle')
+    }
+    else {
+        setPropertyValue('meta.state', 'Battle')
+    }
+}
+
 function updatePropertiesForPartyPokemon(path) {
     const generation = Number(getPropertyValue("game.generation"))
     
